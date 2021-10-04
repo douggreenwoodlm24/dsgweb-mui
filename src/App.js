a20510fdef040d945305f538c8a31d3b3330f38e
Doug <douglas_greenwood@hotmail.com>
2021-10-04 13:04:38 +0100

    Prior to adding Dark Mode

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import Portfolio from "./components/Portfolio";
+import Home from "./pages/Home";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Portfolio />
+      <Home />
     </div>
   );
 }
